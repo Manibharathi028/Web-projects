37c59f1b688730bd2dd84547ee7755b195273efe
Manibharathi028 <168360131+Manibharathi028@users.noreply.github.com>
2024-07-12 23:15:41 +0530

    Add files via upload

---
+++ form validation page/javascript/script.js
@@ -0,0 +1,21 @@
+function validateForm(){
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    document.getElementById('nameError').innerHTML='';
+    document.getElementById('emailError').innerHTML='';
+
+    if(name===''){
+        document.getElementById('nameError').innerHTML='Name is required';
+        return false;
+
+    }
+
+    var emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        document.getElementById('emailError').innerHTML='Invalid email'
+        return false;
+    }
+
+    alert('Form submitted successfully!');
+    return true;
+}
